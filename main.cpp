dac5f3bc7c2db0fc5af70427deec45802e62ea85
syuuchicho <k021g1478@g.neec.ac.jp>
2022-12-01 11:17:16 +0900

    2-2char型

--- main.cpp
+++ main.cpp
@@ -1,33 +1,35 @@
 #include<stdio.h>
 #include<stdlib.h>
-
+#include<string.h>
 
 using namespace std;
 
 
 typedef struct cell
 {
-	int val;
+	char str[8];
 	struct cell* next;
 }CELL;
 
-void create(CELL* head, int val);	//データを追加する関数のプロトタイプ宣言　
+void create(CELL* endCell, const char* buf);	//データを追加する関数のプロトタイプ宣言　
 void index(CELL* head);	//一覧を表示する関数のプロトタイプ宣言
 
 int main()
 {
-	int val;
+	char str[8];
 
 	CELL head;
 	head.next = nullptr;
 
 	while (true)
 	{
-		scanf_s("%d", &val);
+		printf("数字を入力してください\n");
+		scanf_s("%s", str,8);
 
 		//最後尾にセルを追加
-		create(&head, val);
+		create(&head, str);
 
+		printf("一覧\n");
 		//リスト一覧の表示
 		index(&head);
 	}
@@ -35,26 +37,26 @@ int main()
 	return 0;
 }
 
-void create(CELL*head,int val)
+void create(CELL* endCell, const char*buf)
 {
-	CELL *beta;
+	CELL* newCell;
 	//新規作成するセル分のメモリを確保する
-	beta = (CELL*)malloc(sizeof(CELL));
+	newCell = (CELL*)malloc(sizeof(CELL));
 
-	beta->val = val;
-	beta->next = nullptr;
+	strcpy_s(newCell->str, 8, buf);
+	newCell->next = nullptr;
 
 	//最後(最新)のセルのアドレスの1つめの処理は引数から持ってきた
 	//リストのうち最初にセルのアドレスが該当する
-	while (head->next!=nullptr){
-		head = head->next;
+	while (endCell->next != nullptr) {
+		endCell = endCell->next;
 	}
-	head->next = beta;
+	endCell->next = newCell;
 }
 
 void index(CELL* head) {
-	while (head->next!=nullptr){
+	while (head->next != nullptr) {
 		head = head->next;
-		printf("%d\n", head->val);
+		printf("%s\n", head->str);
 	}
 }
