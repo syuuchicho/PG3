abcc9a5928b7271710e29714cd3203761a8b3617
syuuchicho <k021g1478@g.neec.ac.jp>
2022-12-02 21:55:58 +0900

    評価課題02_02双方向リスト(未完)

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-
+//双方向リストの構造体の定義
 typedef struct cell
 {
 	int val;
@@ -12,44 +12,79 @@ typedef struct cell
 }CELL;
 
 void create(CELL* currentCell, int val);	//データを追加する関数のプロトタイプ宣言　
-void index(CELL* endCell);	//一覧を表示する関数のプロトタイプ宣言
+void index(CELL* endCell,int indexMenu);	//一覧を表示する関数のプロトタイプ宣言
 CELL* getInsertCellAddress(CELL* endCELL, int iterator);
 
 int main()
 {
-	int iterator;
-	int inputValue;
+	int menu = 0;
+	int indexMenu = 0;
+	int iterator;			//何番目のセル
+	int inputValue;			//挿入する値
 	CELL* insertCell;
 
-
+	//先頭のセルの宣言
 	CELL head;
 	head.next = nullptr;
 	head.prev = nullptr;
 
 	while (true)
 	{
-		printf("何番目のセルの後ろに挿入しますか?\n");
-		scanf_s("%d", &iterator);
+		switch (menu)
+		{
+		case 0:
+			printf("[要素の操作]\n");
+			printf("1.要素の表示\n");
+			printf("2.要素の挿入\n");
+			printf("3.要素の編集\n");
+			printf("4.要素の削除\n");
+			printf("5.要素の並び替え(オプション)\n\n");
+			printf("--------------------------------------------\n");
+			printf("操作を選択してください\n");
+			scanf_s("%d", &menu);
+			break;
+		case 1:
+			printf("要素の表示\n");
+			printf("1.要素の一覧表示\n");
+			printf("2.順番を指定して要素を表示\n");
+			printf("9.要素操作に戻る\n\n");
 
-		printf("挿入する値を入力してください\n");
-		scanf_s("%d", &inputValue);
+			printf("操作を選択してください\n");
+			scanf_s("%d", &indexMenu);
+			if (indexMenu == 9)
+			{
+				menu = 0;
+				break;
+			}
+			//リスト一覧の表示
+			index(&head,indexMenu);
+			break;
+		case 2:
+			printf("何番目のセルの後ろに挿入しますか?\n");
+			scanf_s("%d", &iterator);
+			printf("挿入する値を入力してください\n");
+			scanf_s("%d", &inputValue);
+			//任意のセルの後ろに追加
+			insertCell = getInsertCellAddress(&head, iterator);
+			create(insertCell, inputValue);
+			menu = 0;
+			break;
+		default:
+			break;
+		}
 
-		//最後尾にセルを追加
-		insertCell = getInsertCellAddress(&head,iterator);
-		create(insertCell,inputValue);
 
-		printf("入力された値一覧\n");
-		//リスト一覧の表示
-		index(&head);
+
 	}
 	return 0;
 }
 
 CELL* getInsertCellAddress(CELL* endCELL, int iterator) {
-	for (int i = 0; i < iterator; i++){
-		if (endCELL->next){
+	for (int i = 0; i < iterator; i++) {
+		if (endCELL->next) {
 			endCELL = endCELL->next;
-		}else{
+		}
+		else {
 			break;
 		}
 	}
@@ -66,7 +101,7 @@ void create(CELL* currentCell, int val)
 	newCell->prev = currentCell;
 	newCell->next = currentCell->next;
 
-	if (currentCell->next){
+	if (currentCell->next) {
 		CELL* nextCell = currentCell->next;
 		nextCell->prev = newCell;
 	}
@@ -74,16 +109,24 @@ void create(CELL* currentCell, int val)
 	currentCell->next = newCell;
 }
 
-void index(CELL* endCell) {
-	int no = 1;
+void index(CELL* endCell,int indexMenu) {
+	int no = 0;
 	//nextに何か値が入っているかぎり出力
+	printf("要素一覧:{\n");
 	while (endCell->next != nullptr) {
 		endCell = endCell->next;
-		//printf("No.%d\n", no);
-		printf("%p\n", endCell->prev);
-		printf("%5d\n", endCell->val);//5桁まで右揃え
-		printf("(%p)\n", endCell);
-		printf("%p\n", endCell->next);
+		printf("%d:", no);
+		//printf("%p\n", endCell->prev);
+		printf("%d\n", endCell->val);//5桁まで右揃え
+		//printf("(%p)\n", endCell);
+		//printf("%p\n", endCell->next);
 		no++;
 	}
+	/*if (endCell->next==nullptr)
+	{
+		printf("表示できる要素がありません\n");
+	}*/
+	printf("}\n\n");
+	printf("要素数:%d\n\n", no);
+
 }
